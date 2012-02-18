7166fada82e06f0b7adfa7a296115a5a63a5430c
oappi <compvter@gmail.com>
2012-02-18 23:09:17 +0200

    Made jsonretrive class that downloads json file as string... May not be the best solution since it downloads whole file.

--- src/fi/aalto/lounaspaikka/LounaspaikkaActivity.java
+++ src/fi/aalto/lounaspaikka/LounaspaikkaActivity.java
@@ -3,11 +3,17 @@ package fi.aalto.lounaspaikka;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+
 public class LounaspaikkaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+ jsonretrive json = new jsonretrive(); 
+	json.fetchdata();
+	
+	}
+
 }
