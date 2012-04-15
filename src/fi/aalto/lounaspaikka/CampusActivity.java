7a890caebe88284529ab96a040365931c5be4987
KK Chan <philipckk@gmail.com>
2012-04-15 21:11:20 +0800

    Restaurant page linked to real data without specific icons Campus selection page added

--- src/fi/aalto/lounaspaikka/CampusActivity.java
+++ src/fi/aalto/lounaspaikka/CampusActivity.java
@@ -1,14 +1,51 @@
 package fi.aalto.lounaspaikka;
 
+import java.util.Vector;
+
 import fi.aalto.lounaspaikka.R;
+import fi.aalto.lounaspaikka.objectfiles.ObjectsContainer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CampusActivity extends Activity{
+	static String nowCampus = "Otaniemi";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.campus);
-		
+		Spinner spinner = (Spinner) findViewById(R.id.campusSpinner);
+		Vector<String> campusName = new Vector<String>();
+		String temp;
+		for (int i=0;i<ObjectsContainer.restaurants.size();i++){
+			temp=ObjectsContainer.restaurants.get(i).campus;
+			if (!campusName.contains(temp))
+				campusName.add(temp);
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,campusName);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setSelection(campusName.indexOf(nowCampus));
+		spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+			@Override
+			public void onItemSelected(AdapterView arg0, View arg1,
+					int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				if (!nowCampus.equals(arg0.getSelectedItem().toString())){
+					nowCampus = arg0.getSelectedItem().toString();
+					Toast.makeText(getApplicationContext(), "Campus changed to "+nowCampus, Toast.LENGTH_LONG).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "Please select a campus", Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
