508786a33b09db0bd996413e1b05513bdc34c39c
valeriyv <v.volodenkov@gmail.com>
2012-04-21 21:13:46 +0300

    Added data storage. Refactored data loading logic. App first check local storage and if no data or its old then gets it from the web. Also this runs in a separate thread.

--- src/fi/aalto/lounaspaikka/LounaspaikkaActivity.java
+++ src/fi/aalto/lounaspaikka/LounaspaikkaActivity.java
@@ -2,33 +2,46 @@ package fi.aalto.lounaspaikka;
 
 
 
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import fi.aalto.lounaspaikka.JsonRetrievalAndTransform.RetrivedDataAndGenerateObject;
-import fi.aalto.lounaspaikka.objectfiles.ObjectsContainer;
-import fi.aalto.lounaspaikka.objectfiles.Restaurant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import fi.aalto.lounaspaikka.JsonRetrievalAndTransform.RetrivedDataAndGenerateObject;
+import fi.aalto.lounaspaikka.objectfiles.ObjectsContainer;
+import fi.aalto.lounaspaikka.objectfiles.Restaurant;
+import fi.aalto.lounaspaikka.objectfiles.db.RestaurantDbEntity;
+import fi.aalto.lounaspaikka.objectfiles.db.RestaurantsDataSource;
 
 
 
 public class LounaspaikkaActivity extends Activity {
+	 private RestaurantsDataSource dataSource;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		RetrivedDataAndGenerateObject data = new  RetrivedDataAndGenerateObject();
-		data.Storetomem();
-		for(int i=0; i<ObjectsContainer.restaurants.size(); i++) {
-			Restaurant res = ObjectsContainer.restaurants.get(i);
-			System.out.println("Reastaurant: " + res.name);
-		}
 		
+		dataSource = new RestaurantsDataSource(this);
+		
+		//run data loading in different thread to avoid UI locking
+		 new Thread(new Runnable() {
+			    public void run() {    
+			    	loadData();
+			    }
+			  }).start();
+	
+ 	 
 		Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
 
@@ -39,10 +52,103 @@ public class LounaspaikkaActivity extends Activity {
 		   }
 
 		}, 2000);
-//		 Intent intent = new Intent(this, DashboardActivity.class);
-//		 this.startActivity(intent);
-
+ 
 	}
+	
+	/**
+	 * download data  from the Internet
+	 */
+	private void getDataFromWeb(){
+		RetrivedDataAndGenerateObject data = new  RetrivedDataAndGenerateObject();
+		data.fechData();
+		data.generateObjects();
+		data.Storetomem();
+		saveDataToLocalStorage(data.getJsonData());
+		Log.w("log1","loaded from web");
+	}
+	
+	/**
+	 * 
+	 * @param data
+	 */
+	private void saveDataToLocalStorage(String data){
+		
+		 	dataSource = new RestaurantsDataSource(this);
+	        dataSource.open();
+	        
+	        String date;
+	        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	        date = dateFormat.format(new Date());
+	        dataSource.SaveData(data, date);
+	        dataSource.close();
+	}
+	
+	/**
+	 * Tries to load data from local storage first. If not data available gets it from the Internet
+	 */
+	private void  loadData(){
+		
+		RestaurantDbEntity restDbEntity = loadDataFromLocalStorage();
+		//incorrect data in local storage
+		if (restDbEntity == null)
+			{getDataFromWeb();
+			return;
+			}
+		
+		//Check if data up to date
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(restDbEntity.getDate());
+		Calendar todayCalendar = Calendar.getInstance();
+		todayCalendar.setTime(new Date());
+		if( calendar.get(Calendar.WEEK_OF_YEAR) != todayCalendar.get(Calendar.WEEK_OF_YEAR)){ 
+			getDataFromWeb();
+			return;
+		}
+		
+		//Pass json from local storage to generate objects
+		RetrivedDataAndGenerateObject data = new  RetrivedDataAndGenerateObject();
+		data.setJsonData(restDbEntity.getRestaurantData());
+		data.generateObjects();
+		data.Storetomem();
+		Log.w("log1","loaded from local storage");
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private RestaurantDbEntity loadDataFromLocalStorage(){
+		RestaurantDbEntity restDbEntity = new RestaurantDbEntity();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		String date;
+	        
+		dataSource = new RestaurantsDataSource(this);
+        dataSource.open();
+        
+        dataSource.getAllData();
+        List<String> values = dataSource.getDataList();
+        List<String> dates = dataSource.getDates();
+        Log.w("log1", values.size() + " size");
+        dataSource.close();
+        
+        if(values.isEmpty() || dates.isEmpty())
+        	return null;
+        
+        try {
+			restDbEntity.setDate(dateFormat.parse(dates.get(0)));
+			
+		} catch (ParseException e) {
+			return null;
+		}
+        
+        restDbEntity.setRestaurantData(values.get(0));
+        
+        return restDbEntity;
+        
+        
+	}
+		
 }
 
 
---
+++ src/fi/aalto/lounaspaikka/objectfiles/db/RestaurantDbEntity.java
@@ -0,0 +1,23 @@
+package fi.aalto.lounaspaikka.objectfiles.db;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RestaurantDbEntity {
+	String restaurantData;
+	public String getRestaurantData() {
+		return restaurantData;
+	}
+	public void setRestaurantData(String restaurantData) {
+		this.restaurantData = restaurantData;
+	}
+	public Date getDate() {
+		return date;
+	}
+	
+	Date date;
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
