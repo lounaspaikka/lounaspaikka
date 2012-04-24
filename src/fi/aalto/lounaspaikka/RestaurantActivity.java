0127c9eb6c2df18666dea1bb8a0506c12f0e38ab
oappi <compvter@gmail.com>
2012-04-24 23:13:08 +0300

    times now show on restaurant activity

--- src/fi/aalto/lounaspaikka/RestaurantActivity.java
+++ src/fi/aalto/lounaspaikka/RestaurantActivity.java
@@ -1,7 +1,7 @@
 package fi.aalto.lounaspaikka;
 
 
-import java.util.Calendar;
+//import java.util.Calendar;
 
 import fi.aalto.lounaspaikka.objectfiles.ObjectsContainer;
 import fi.aalto.lounaspaikka.objectfiles.Restaurant;
@@ -23,20 +23,28 @@ public class RestaurantActivity extends Activity{
 		String restName = myIntent.getStringExtra("restName");
 		//TO BE CHANGED TO SPECIFIC ICON
 		restIcon.setImageResource(R.drawable.respic);
-		
 		Restaurant nowRest = new Restaurant();
-		
+		int rnumber=0;
 		for (int i=0;i<ObjectsContainer.restaurants.size();i++)
 			if (ObjectsContainer.restaurants.get(i).name.equals(restName)){
 				nowRest = ObjectsContainer.restaurants.get(i);
+				rnumber=i;
 				break;
 			}
+
 		if (nowRest.name!=null){
 			restInfo.append("Name: "+nowRest.name+"\n");
 			restInfo.append("Campus: "+nowRest.campus+"\n");
 			restInfo.append("Address: "+nowRest.location.address+"\n");
 			restInfo.append("Opening hours: \n");
-			if (nowRest.isopen.listOfDays.get(0).opens.equals(""))
+			int opensc=0;
+			int openlistsize=nowRest.isopen.listOfDays.size();
+			while (openlistsize>opensc) {
+				restInfo.append(opens(opensc,rnumber));
+				opensc++;
+			}
+
+			/*	if (nowRest.isopen.listOfDays.get(0).opens.equals(""))
 				restInfo.append("Mon: no data\n");
 			else
 				restInfo.append("Mon: "+nowRest.isopen.listOfDays.get(0).opens+" - "+nowRest.isopen.listOfDays.get(0).closes+"\n");
@@ -64,7 +72,51 @@ public class RestaurantActivity extends Activity{
 				restInfo.append("Sun: no data\n");
 			else
 				restInfo.append("Sun: "+nowRest.isopen.listOfDays.get(6).opens+" - "+nowRest.isopen.listOfDays.get(6).closes+"\n");
+		}*/
+
 		}
-		
 	}
+
+	private String opens(int day, int rnumber) //rnumber is restaurant number
+	{
+		String opens="";
+		String closes="";
+		String openclose="";
+		if (ObjectsContainer.restaurants.get(rnumber).isopen.listOfDays.get(day).opens.equals("") || ObjectsContainer.restaurants.get(rnumber).isopen.listOfDays.get(day).closes.equals("")  ) {
+			openclose= "Hours can't be retrived" + System.getProperty("line.separator");
+		} 
+		else 
+		{
+			opens=ObjectsContainer.restaurants.get(rnumber).isopen.listOfDays.get(day).opens;
+			closes= ObjectsContainer.restaurants.get(rnumber).isopen.listOfDays.get(day).opens;
+			openclose = opens + "-" + closes;
+		}
+		if (day==0) {
+			openclose= "Monday: " +openclose+System.getProperty("line.separator"); 	
+		}
+		 else if (day==1) {		
+			 openclose= "Tuesday: " +openclose+System.getProperty("line.separator"); 	
+		 }
+		else if (day==2) {
+			openclose= "Wednesday: " +openclose+System.getProperty("line.separator"); 		
+		}
+		else if (day==3) {
+			openclose= "Thursday: " +openclose+System.getProperty("line.separator"); 	
+		}
+		else if (day==4) {
+			openclose= "Friday: " +openclose+System.getProperty("line.separator"); 	
+		}
+		else if (day==5) {
+			openclose= "Saturday: " +openclose+System.getProperty("line.separator"); 		
+		}
+		else if (day==6) {
+			openclose= "Sunday: " +openclose+System.getProperty("line.separator"); 	
+		}
+
+
+
+		return openclose;
+	}
+
+
 }
