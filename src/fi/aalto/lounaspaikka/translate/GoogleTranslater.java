70a36321bb6daacec9866ce525f630c8b1f27bfc
Jussi Patana <jussi.patana@tkk.fi>
2012-05-03 22:59:39 +0300

    Added translator function.

---
+++ src/fi/aalto/lounaspaikka/translate/GoogleTranslater.java
@@ -0,0 +1,26 @@
+package fi.aalto.lounaspaikka.translate;
+
+import com.google.api.GoogleAPI;
+import com.google.api.GoogleAPIException;
+import com.google.api.translate.Language;
+import com.google.api.translate.Translate;
+
+public class GoogleTranslater {
+
+	public static String Translate(String input) {
+		if (input.length() == 0){
+			return "";
+		}
+		String output = input;
+		
+		GoogleAPI.setHttpReferrer("http://localhost");
+        GoogleAPI.setKey("KEY");
+		try {
+			output = Translate.DEFAULT.execute(input, Language.FINNISH, Language.ENGLISH);
+		} catch (GoogleAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return output;
+	}
+}
